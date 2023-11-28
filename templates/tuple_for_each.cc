2dff150c0e30976535d1003316e842c3fd01250a
HongboLiu <lhbf@qq.com>
2023-11-28 15:55:25 +0800

    refactor tuple for each

--- templates/tuple_for_each.cc
+++ templates/tuple_for_each.cc
@@ -5,22 +5,18 @@
 #include <utility>
 
 template<std::size_t I = 0, typename FuncT, typename ... Tp>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-for_each(std::tuple<Tp...>&, FuncT) // Unused arguments are given no names.
-{
-}
+void ForEach(std::tuple<Tp...>&, FuncT) requires(I == sizeof...(Tp)) {}
 
 template<std::size_t I = 0, typename FuncT, typename ... Tp>
-inline typename std::enable_if<I < sizeof...(Tp), void>::type
-for_each(std::tuple<Tp...> &t, FuncT f)
-{
-    f(std::get<I>(t));
-    for_each<I + 1, FuncT, Tp...>(t, f);
+void ForEach(std::tuple<Tp...> &t, FuncT f) requires(I < sizeof...(Tp)) {
+  f(std::get<I>(t));
+  ForEach<I + 1, FuncT, Tp...>(t, f);
 }
 
-int main()
-{
+int main() {
     typedef std::tuple<int, float, double> T;
     T t = std::make_tuple(2, 3.14159F, 2345.678);
-    for_each(t, [](auto& i){std::cout<<i<<std::endl;});
+    ForEach(t, [](auto i) {
+      std::cout<<i<<std::endl;
+    });
 }
