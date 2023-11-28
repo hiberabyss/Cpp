47d402107a18ce11ae2bb93764298ed50b4f63a2
HongboLiu <lhbf@qq.com>
2023-11-28 16:31:59 +0800

    support ForEachType

--- templates/tuple_for_each.cc
+++ templates/tuple_for_each.cc
@@ -13,10 +13,22 @@ void ForEach(std::tuple<Tp...> &t, FuncT f) requires(I < sizeof...(Tp)) {
   ForEach<I + 1, FuncT, Tp...>(t, f);
 }
 
+template<typename T>
+void Typeid() {
+  std::cout << typeid(T).name() << std::endl;
+}
+
+template<typename... ARGS>
+void ForEachType() {
+  ((Typeid<ARGS>()), ...);
+}
+
 int main() {
     typedef std::tuple<int, float, double> T;
     T t = std::make_tuple(2, 3.14159F, 2345.678);
     ForEach(t, [](auto i) {
       std::cout<<i<<std::endl;
     });
+
+    ForEachType<int, float, double>();
 }
